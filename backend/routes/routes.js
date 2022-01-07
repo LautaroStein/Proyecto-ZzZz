865b130d7c85a29453e641038e178b490510fbff
franco sosa <gonza123e@gmail.com>
2022-01-07 00:16:14 -0300

    controlador

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,4 +1,12 @@
 const Router = require("express").Router();
+const NftControllers=require("../controllers/NftController")
+const{getAllNft,loadUnNft, modifyAnNft,getOneNft }=NftControllers
+Router.route('/nft')
+.get(getAllNft)
+.post(loadUnNft)
+Router.route("/nft/:id")
+.put( modifyAnNft) 
+.get(getOneNft)
 
 
 
