0fe1a14f264ec45cb69a704d69632c2cf1e7b6d0
PedroSalduna <pewemes@gmail.com>
2022-01-07 10:56:11 -0300

    commit vacio

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -37,7 +37,7 @@ const SignUp = () => {
         })
         .catch((error) => {
             console.log(error)
-            alert('Something went wrong! Come back later!')
+            alert('Something went wrong, come back later.')
   
         })
   
