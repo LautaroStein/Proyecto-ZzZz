e7c749f2909ee2f2001644aa96f9af4b8dbbdee5
19037908 <moraga.hermosilla@hotmail.com>
2022-01-07 23:11:58 -0300

    fix play/pause gif

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
 const Home = () => {
-    const [asset, setAsset] = useState('./assets/git-photo.jpeg')
+    const [asset, setAsset] = useState('./assets/9778e9361fabdb7fd6eded5ec35102f5.gif')
     const [enter, setEnter] = useState(false)
     const [play, setPLay] = useState(false)
     return (
@@ -22,17 +22,17 @@ const Home = () => {
                     <div className="contenedor-nft-real">
                         <div className="contenedor-nft-total">
                             <div style={{ backgroundImage: `url(${asset})` }} onMouseEnter={() => setEnter(true)} onMouseLeave={() => setEnter(false)} className="image">
-                                {(enter && !play) &&
-                                    <div onClick={() => {
-                                        setAsset('./assets/9778e9361fabdb7fd6eded5ec35102f5.gif')
-                                        setPLay(true)
-                                    }}><i className="fas fa-play"></i></div>
-                                }
                                 {(enter && play) &&
                                     <div onClick={() => {
-                                        setAsset('./assets/git-photo.jpeg')
+                                        setAsset('./assets/9778e9361fabdb7fd6eded5ec35102f5.gif')
                                         setPLay(false)
-                                    }}><i className="fas fa-pause"></i></div>
+                                    }}><i style={{ fontSize: '1.5rem' }} className="fas fa-play"></i></div>
+                                }
+                                {(enter && !play) &&
+                                    <div onClick={() => {
+                                        setAsset('./assets/gif-photo.jpeg')
+                                        setPLay(true)
+                                    }}><i style={{ fontSize: '1.5rem' }} className="fas fa-pause"></i></div>
                                 }
                             </div>
                             <div className="texto-nft-home">
