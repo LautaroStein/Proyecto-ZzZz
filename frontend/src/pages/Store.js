d1b8ae3e02ce2c27c73f2da0331888d44e34e332
DeveloperAdrian <developer.adrianh@gmail.com>
2022-01-09 17:38:12 -0300

    nada

--- frontend/src/pages/Store.js
+++ frontend/src/pages/Store.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import FiltroStore from '../components/FiltroStore'
+
+
 
 const Store = () => {
     return (
-        <div>
-            <FiltroStore/>
+        <div >
+            
+    
+            
         </div>
     )
 }
