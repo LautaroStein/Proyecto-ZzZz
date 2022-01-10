7c16ba10d8d950d82cf968c5c3a01f7d63db4b6f
facundotecheira <facundotecheira00@gmail.com>
2022-01-10 13:59:19 -0300

    .

--- frontend/src/components/FiltroStore.js
+++ frontend/src/components/FiltroStore.js
@@ -4,11 +4,9 @@ const FiltroStore = () => {
 
   const BuscarNFT = useRef();
 
-
   const [categoria, setCategoria] = useState("all");
   const [rareza, setRareza] = useState("all");
-
-
+  const [desabilitado,setDesabilitado] = useState(false)
 
   const api = {
     categorias: [
@@ -27,6 +25,7 @@ const FiltroStore = () => {
 
     ]
   }
+
   const [nftDefault, setNftDefault] = useState(api.categorias);
   const [nft, setNft] = useState(api.categorias);
   const [auxiliar, setAuxiliar] = useState(nft)
@@ -37,27 +36,85 @@ const FiltroStore = () => {
 
 
   const fetchear = () => {
-    if (categoria !== "all") {
+
+    if (categoria !== "all" && rareza == "all") {
       let resultado = nftDefault.filter((e) => e.nameCategoria == categoria)
       setNft(resultado)
       setAuxiliar(resultado)
-    } else {
+
+      if (resultado.length == 0){
+        setDesabilitado(true)
+      }else{
+        setDesabilitado(false)
+      }
+    }
+
+    if (categoria !== "all" && rareza !== "all") {
+      let resultado = nftDefault.filter((e) => e.nameCategoria == categoria && e.rareza == rareza)
+
+      setNft(resultado)
+      setAuxiliar(resultado)
+
+      if (resultado.length == 0){
+        setDesabilitado(true)
+      }else{
+        setDesabilitado(false)
+      }
+    }
+
+    if (categoria == "all" && rareza !== "all") {
+      let resultado = nftDefault.filter((e) => e.rareza == rareza)
+
+      setNft(resultado)
+      setAuxiliar(resultado)
+
+      if (resultado.length == 0){
+        setDesabilitado(true)
+      }else{
+        setDesabilitado(false)
+      }
+    }
+
+    if (categoria == "all" && rareza == "all") {
+
       setNft(api.categorias)
       setAuxiliar(api.categorias)
+
     }
 
   }
 
+
+
   const fetchearRareza = () => {
-    
-    if(rareza !== "all" ){
-      let resultado = nft.filter((e) => {
-      return e.rareza.includes(rareza)
-    })
-    setAuxiliar(resultado)
-    }else{
-      setAuxiliar(nft)
+
+    if (categoria == "all" && rareza !== "all") {
+      let resultado = nft.filter((e) => e.rareza == rareza)
+      setAuxiliar(resultado)
+      if (resultado.length == 0){
+        setDesabilitado(true)
+      }else{
+        setDesabilitado(false)
+      }
     }
+
+    if (categoria !== "all" && rareza !== "all") {
+      let resultado = nftDefault.filter((e) => e.nameCategoria == categoria && e.rareza == rareza)
+      setAuxiliar(resultado)
+      if (resultado.length == 0){
+        setDesabilitado(true)
+      }else{
+        setDesabilitado(false)
+      }
+    }
+
+    if (categoria == "all" && rareza == "all") {
+
+      setNft(api.categorias)
+      setAuxiliar(api.categorias)
+
+    }
+
 
   }
 
@@ -65,18 +122,20 @@ const FiltroStore = () => {
   const fetchearPorNombre = () => {
     let value = BuscarNFT.current.value
 
-    let resultado = nft.filter((e) => {
-      return e.name.includes(value)
-    })
-    setAuxiliar(resultado)
+   
+      let resultado = nft.filter((e) => {
+        return e.name.includes(value)
+      })
+      setAuxiliar(resultado)
+    
 
   }
 
 
 
+
   return (
     <div style={{ color: "white", margin: "50rem", border: "1px solid white" }}>
-
       <h1>hola</h1>
       <input
         className="input-pelis"
@@ -84,6 +143,7 @@ const FiltroStore = () => {
         ref={BuscarNFT}
         placeholder="Buscar una pelicula..."
         onChange={fetchearPorNombre}
+        disabled={desabilitado}
       />
       <form
         onClick={fetchear}
@@ -97,12 +157,12 @@ const FiltroStore = () => {
               )
             })
           }
-          
-        
-        </select>
-        </form>
 
-        <form
+
+        </select>
+      </form>
+
+      <form
         onClick={fetchearRareza}
       >
 
@@ -118,12 +178,15 @@ const FiltroStore = () => {
         </select>
 
       </form>
-      
+
 
       {
         auxiliar.map((e) => {
           return (
-            <h3>{e.name}</h3>
+            <>
+              <h3>{e.name}</h3>
+
+            </>
           )
         })
       }
@@ -132,5 +195,4 @@ const FiltroStore = () => {
     </div>
   )
 }
-
 export default FiltroStore
