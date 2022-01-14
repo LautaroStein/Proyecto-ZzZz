d3010ae837194f0b2505bf397f56fc71191ad4a3
matiassosa99 <sos.mati16@gmail.com>
2022-01-14 18:02:00 -0300

    redux en app.js + actions y reducers (sin implementar)

---
+++ native/src/redux/reducers/mainReducer.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux"
+import userReducers from './userReducers'
+import nftReducers from './nftReducers'
+
+const mainReducer = combineReducers({
+    userReducers,
+    nftReducers,
+})
+
+export default mainReducer
