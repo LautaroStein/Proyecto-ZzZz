65416c3223d73ef9371169f80dd22953ce1c56ee
matiassosa99 <sos.mati16@gmail.com>
2022-01-19 02:59:50 -0300

    cart funcionando, no funciona input de discountCode, falta agregar boton paypal

--- native/src/screens/Cart.js
+++ native/src/screens/Cart.js
@@ -1,37 +1,96 @@
 import React from 'react';
-import { View, Text, ScrollView, StyleSheet, Image, Button } from 'react-native'
+import { View, Text, ScrollView, StyleSheet, TouchableOpacity, TextInput } from 'react-native'
 // import { BsArrowDownCircle, BsFileEaselFill } from "react-icons/bs"
-// import { useEffect, useState} from "react"
+import { useEffect, useState} from "react"
 import { connect } from "react-redux"
-// import CardNFT from "../components/Cart/CardNFT"
+import CardNFTNative from "../components/Cart/CardNFTNative"
 import cartActions from "../redux/actions/cartActions"
 // import PayPal from "../components/Cart/PayPalForm/PayPal"
 
-const Cart = () => {
+const Cart = ({cart, clearCartAll, deleteOneNFT, setTotalPrice}) => {
+
+    const [ code, setCode ] =  useState(false)
+    const [ discount, setDiscount ] = useState("")
+    
+    const descounts = [ "#246FIRSTPURCHASENFTCART", "#AGUANTELOSNFT2022", "ESTASFULLPICADOPAA" ]
+    
+    let subTotal = 0
+
+    const [ total, setTotal ] = useState(subTotal)
+
+    useEffect(()=> {
+        discountes()
+    }, [cart, subTotal])
+    
+    cart.forEach(item => {
+        return subTotal = subTotal + item.price
+    })   
+
+    console.log(subTotal)
+
+    const discountes = () => {
+        if(descounts.some(item => item === discount)){
+            setTotal(subTotal - (subTotal * 0.1)) 
+        }else{
+            setTotal(subTotal)
+        }
+    }   
+
+    useEffect(() => {
+        setTotalPrice(total)
+    }, [total])
+
     return (
  <ScrollView style={cartStyle.mainView}>
-     <View >
+     <View style={cartStyle.mainContainer}>
             <View  style={cartStyle.view1}>
                 <Text style={cartStyle.title}>Get 10% off your first NFT purchase by entering the following discount code.</Text>                
             </View>
             <View>
-                    <View style={cartStyle.CardNFTStyle.contenedor} >
-                    <View style={cartStyle.CardNFTStyle.contenedorTitulo}>
-                        <Text style={cartStyle.CardNFTStyle.text}>name nft</Text>
-                    </View>
-                    {/* <Image style={cartStyle.CardNFTStyle.imagenNft} source={img} alt="nftImg" /> */}
-                    <View style={cartStyle.CardNFTStyle.contenedorDatos}>
-                            <Text style={cartStyle.CardNFTStyle.text}>Class: </Text>
-                            <Text style={cartStyle.CardNFTStyle.text}>Type: </Text>
-                            <Text style={cartStyle.CardNFTStyle.text}>100 ETH</Text>
-                    </View>
-                    <View style={cartStyle.CardNFTStyle.contenedorButtons}>
-                        <Button  style={cartStyle.CardNFTStyle.button} title="delete 🗑" />
-                    </View>
-                </View>
-        </View>
+                <Text style={cartStyle.textCart}>YOUR CART ({cart.length})</Text>
+                <TouchableOpacity onPress={() => clearCartAll()} style={cartStyle.buttonClearCart} activeOpacity={0.2} underlayColor="gray">
+                  <Text style={cartStyle.buttonText}> CLEAR CART </Text>
+                </TouchableOpacity>
+            </View>
+            <View>
+                {cart.map((element,i) => {
+                    return <CardNFTNative key={i} data={element} remove={deleteOneNFT} />
+                })}
+            </View>
+            
         <View style={cartStyle.summary}>
-        <Text style={cartStyle.title}>SUMMARY</Text> 
+            <Text style={cartStyle.title2}>SUMMARY</Text> 
+        </View>
+        <View>
+            <Text style={cartStyle.text}>Do you have a promo code?</Text>
+            
+                <TouchableOpacity onPress={() => {setCode(!code)}} style={cartStyle.buttonDiscount} activeOpacity={0.2} underlayColor="gray">
+                  {!code ? <Text style={cartStyle.buttonText}>YES!</Text> : <Text style={cartStyle.buttonText}>NO!</Text>}
+                </TouchableOpacity>   
+            {code ?
+                <>
+                    <TextInput
+                        style={cartStyle.textInput}
+                        placeholder="Code"
+                        onChangeText={text => setDiscount(text)}
+                        defaultValue={code}
+                    />
+                    <TouchableOpacity onPress={()=> discountes()} style={cartStyle.applyDiscount} activeOpacity={0.2} underlayColor="gray">
+                        <Text style={cartStyle.buttonText}> Enter </Text>
+                    </TouchableOpacity>
+                </>
+            : null }
+        </View>
+        <View style={cartStyle.subTotalView}>
+            <Text style={cartStyle.title}>SUBTOTAL</Text>
+            <Text style={cartStyle.text}>${subTotal}</Text>
+        </View>
+        <View  style={cartStyle.textAdvice}>
+            <Text style={cartStyle.text}>Once payment is made, no refunds are accepted, the proccess may take 1 to 10 minutes to be completed successfully</Text>
+        </View>
+        <View style={cartStyle.totalView}>
+            <Text style={cartStyle.title}>TOTAL</Text>
+            <Text style={cartStyle.text}>${total}</Text>
         </View>
      </View>
 </ScrollView>
@@ -40,80 +99,81 @@ const Cart = () => {
 
 const cartStyle = StyleSheet.create({
     mainView:{
-        backgroundColor:'black',
+        backgroundColor:'#14141d',
+    },
+    mainContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
     },
     view1:{
             color:'black',
         margin:'1rem',
     },
-    // titleWelcome:{
-    //     color:'white',
-    //     width: '100%',
-    //     height:'0.5rem',
-    //     textAlign: 'center',
-    //     margin: '1rem',
-    // },
     title:{
         color:'white',
         width: '100%',
-        height:'0.5rem',
         textAlign: 'center',
-        margin: '1rem',
         fontSize:'1rem',
         fontWeight:'bold',
     },
- 
-    CardNFTStyle:{
-        contenedor:{
-            width:'95%',
-            display: 'flex',
-            backgroundColor:'blue',
-            alignSelf: 'center',
-            
-        },
-        contenedorTitulo:{
-            width:'95%',
-            display: 'flex',
-            backgroundColor:'blue',
-            alignSelf: 'center',
-            padding:'0.5rem',
-            
-        },
-        contenedorDatos:{
-            width:'95%',
-            padding:'0.5rem',
-            display: 'flex',
-            flexDirection:'row',
-            justifyContent:'space-between',
-            backgroundColor:'green',
-            alignSelf: 'center',
-            
-        },
-        contenedorButtons:{
-            width:'95%',
-            padding:'0.5rem',
-            display: 'flex',
-            flexDirection:'row',
-            justifyContent:'space-around',
-            backgroundColor:'red',
-            alignSelf: 'center',
-            
-        },
-        text:{
-            color:'white',
-            alignSelf: 'center',
-        },
-        imagenNft:{
-            margin:'0.5rem',        
-            width: '250px',
-            height: '250px',
-            alignSelf: 'center',
-        },
-        button:{
-            padding: '0.5rem',
-        },
-    }
-    
+    title2:{
+        color:'white',
+        width: '100%',
+        textAlign: 'center',
+        fontSize:'1rem',
+        fontWeight:'bold',
+        fontSize: 24,
+    },
+    text: {
+        color:'white',
+        alignSelf: 'center',
+        padding: '.5rem'
+    },
+    textAdvice: {
+        textAlign: 'center',
+    },
+    textCart: {
+        color: '#96faaf',
+        fontWeight: 'bold',
+        fontSize: 32,
+    },
+    totalView: {
+        backgroundColor: '#8f0788',
+        width: '90%',
+    },
+    subTotalView: {
+        backgroundColor: '#001a33',
+        width: '90%',
+    },
+    buttonClearCart: {
+        backgroundColor:'#8f0788',
+        borderColor: '#b903b0',
+        borderWidth: 2,
+        borderRadius: 10,
+        margin: '.2rem',
+    },
+    buttonDiscount: {
+        backgroundColor: '#001a33',
+        borderColor: '#1874ec',
+        borderWidth: 2,
+        borderRadius: 10,
+        margin: '.2rem',
+    },
+    applyDiscount: {
+        backgroundColor: '#96faaf50',
+        borderColor: '#96faaf',
+        borderWidth: 2,
+        borderRadius: 10,
+        margin: '.2rem',
+    },
+    buttonText: {
+        color: 'white',
+        textAlign: 'center',
+        padding: '.2rem',
+        fontWeight: 'bold',
+    },
+
 })
 
 
@@ -130,40 +190,6 @@ const mapDispatchToProps = {
 
 export default connect(mapStateToProps, mapDispatchToProps)(Cart)
 
-
-
-// const Cart = ({cart, clearCartAll, deleteOneNFT, setTotalPrice}) => {
-
-//     const [ code, setCode ] =  useState(false)
-//     const [ discount, setDiscount ] = useState("")
-    
-//     const descounts = [ "#246FIRSTPURCHASENFTCART", "#AGUANTELOSNFT2022", "ESTASFULLPICADOPAA" ]
-    
-//     let subTotal = 0
-
-//     const [ total, setTotal ] = useState(subTotal)
-
-//     useEffect(()=> {
-//         discountes()
-//     }, [cart, subTotal])
-    
-//     cart.forEach(item => {
-//         return subTotal = subTotal + item.price
-//     })   
-
-//     console.log(subTotal)
-
-//     const discountes = () => {
-//         if(descounts.some(item => item === discount)){
-//             setTotal(subTotal - (subTotal * 0.1)) 
-//         }else{
-//             setTotal(subTotal)
-//         }
-//     }   
-
-//     useEffect(() => {
-//         setTotalPrice(total)
-//     }, [total])
 
 //     return (
 //         <>
@@ -198,17 +224,3 @@ export default connect(mapStateToProps, mapDispatchToProps)(Cart)
 //             </div>
 //         </>
 //     )
-// }
-
-// const mapStateToProps = (state) => {
-//     return {
-//         cart : state.cartReducers.cart,
-//     }
-// }
-// const mapDispatchToProps = {
-//     clearCartAll : cartActions.removeAllFromCart,
-//     deleteOneNFT : cartActions.removeOneFromCart,
-//     setTotalPrice : cartActions.setTotalPrice
-// }
-
-// export default connect(mapStateToProps, mapDispatchToProps)(Cart)
